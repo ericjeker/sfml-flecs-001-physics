cf8b5a1acc003c3e8d12f6349180fd56dac98c69
Eric Jeker <eric@wowww.ch>
2025-09-13 20:56:35 +0200

    feat: enhance particle system with shooting mechanics, gravity, restitution, and mouse interaction

---
+++ include/core/components/ScreenBoundaries.h
@@ -0,0 +1,9 @@
+// Copyright (c) Eric Jeker. All Rights Reserved.
+
+#pragma once
+
+#include <SFML/Graphics/Rect.hpp>
+
+struct ScreenBoundaries {
+  sf::FloatRect bounds;
+};
--- src/Main.cpp
+++ src/Main.cpp
@@ -11,30 +11,34 @@
 
 #include "core/components/CircleRenderable.h"
 #include "core/components/Physics.h"
+#include "core/components/ScreenBoundaries.h"
 #include "core/components/Transform.h"
 #include "core/components/VerticesRenderable.h"
 #include "core/themes/Nord.h"
 
 namespace {
-struct ScreenBoundaries {
-  sf::FloatRect bounds;
-};
+constexpr float SCREEN_PADDING = 5.f;
+constexpr float SCREEN_WIDTH = 1920.f;
+constexpr float SCREEN_HEIGHT = 1080.f;
+constexpr float PARTICLE_RADIUS = 20.f;
+constexpr sf::Vector2f GRAVITY = {0.f, 9800.f};
+constexpr float RESTITUTION = 0.9f;
 
-const float SCREEN_PADDING = 5.f;
-const float SCREEN_WIDTH = 1920.f;
-const float SCREEN_HEIGHT = 1080.f;
+struct MouseState {
+  sf::Vector2i startPosition;
+};
 }  // namespace
 
 flecs::entity CreateParticleEntity(const flecs::world& world) {
   const auto particle = world.entity()
                             .set<CircleRenderable>({})
                             .set<Transform>({{SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f}})
-                            .set<Physics>({.damping = 0.98f});
+                            .set<Physics>({.damping = 0.98f, .gravity = GRAVITY});
   auto& shape = particle.get_mut<CircleRenderable>().shape;
-  shape.setRadius(20.f);
-  shape.setPointCount(64);
+  shape.setRadius(PARTICLE_RADIUS);
+  shape.setPointCount(16);
   shape.setFillColor(NordTheme::Frost1);
-  shape.setOrigin({20.f, 20.f});
+  shape.setOrigin({PARTICLE_RADIUS, PARTICLE_RADIUS});
 
   return particle;
 }
@@ -79,18 +83,25 @@ auto HandleBoundaryCollision() {
   return [](const flecs::iter& it, size_t, const CircleRenderable& c, Transform& t, Physics& p) {
     const auto screenBounds = it.world().get<ScreenBoundaries>().bounds;
     const auto radius = c.shape.getRadius();
+    bool collided = false;
 
     if (t.position.x - radius < screenBounds.position.x ||
         t.position.x + radius > screenBounds.position.x + screenBounds.size.x) {
+      collided = true;
       p.velocity.x *= -1;
       t.position.x = std::clamp(t.position.x, screenBounds.position.x + radius,
                                 screenBounds.position.x + screenBounds.size.x - radius);
     } else if (t.position.y - radius < screenBounds.position.y ||
                t.position.y + radius > screenBounds.position.y + screenBounds.size.y) {
+      collided = true;
       p.velocity.y *= -1;
       t.position.y = std::clamp(t.position.y, screenBounds.position.y + radius,
                                 screenBounds.position.y + screenBounds.size.y - radius);
     }
+
+    // Restitution
+    if (collided)
+      p.velocity *= RESTITUTION;
   };
 }
 
@@ -108,12 +119,47 @@ auto DrawCircleShape(sf::RenderWindow& window) {
   };
 }
 
+void ShotParticleOnMouseReleased(const flecs::world& world, const sf::Event::MouseButtonReleased* mouseReleased) {
+  // Record the position of the release
+  const auto startPosition = world.get<MouseState>().startPosition;
+  const auto releasePosition = mouseReleased->position;
+
+  // Calculate the vector between the two clicks
+  auto delta = releasePosition - startPosition;
+  delta *= 10;
+
+  // Generate a particle and give it that acceleration
+  auto particle = CreateParticleEntity(world);
+  particle.get_mut<Transform>().position =
+      sf::Vector2f{static_cast<float>(startPosition.x), static_cast<float>(startPosition.y)};
+  particle.get_mut<Physics>().velocity = sf::Vector2f{static_cast<float>(delta.x), static_cast<float>(delta.y)};
+
+  // Clean-up
+  world.remove<MouseState>();
+}
+
+void UpdateThrowLine(const flecs::world& world, const sf::Event::MouseMoved* mouseMoved) {
+  if (!world.has<MouseState>())
+    return;
+
+  const auto& [startPosition] = world.get<MouseState>();
+  const auto& currentPosition = mouseMoved->position;
+  world.set<VerticesRenderable>({
+      .primitiveType = sf::PrimitiveType::Lines,
+  });
+  world.get_mut<VerticesRenderable>().vertices.push_back(
+      sf::Vertex{.position = sf::Vector2f(startPosition), .color = NordTheme::Frost1});
+  world.get_mut<VerticesRenderable>().vertices.push_back(
+      sf::Vertex{.position = sf::Vector2f(currentPosition), .color = NordTheme::Frost1});
+}
+
 int main() {
   sf::ContextSettings settings;
   settings.antiAliasingLevel = 4;
 
-  auto window = sf::RenderWindow(sf::VideoMode({static_cast<unsigned>(1920.f), static_cast<unsigned>(1080.f)}),
-                                 "CMake SFML Project", sf::Style::None, sf::State::Windowed, settings);
+  auto window =
+      sf::RenderWindow(sf::VideoMode({static_cast<unsigned>(SCREEN_WIDTH), static_cast<unsigned>(SCREEN_HEIGHT)}),
+                       "CMake SFML Project", sf::Style::None, sf::State::Windowed, settings);
   window.setFramerateLimit(144);
 
   // the unique flecs world
@@ -151,7 +197,21 @@ int main() {
       } else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
         if (keyPressed->code == sf::Keyboard::Key::Escape) {
           window.close();
+        } else if (keyPressed->code == sf::Keyboard::Key::R) {
+          // Restart the simulation
+          world.query<Transform, Physics>().each([](Transform& t, Physics& p) {
+            t.position = {SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f};
+            p.velocity = {0.f, 0.f};
+          });
         }
+      } else if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>()) {
+        UpdateThrowLine(world, mouseMoved);
+      } else if (const auto* mousePressed = event->getIf<sf::Event::MouseButtonPressed>()) {
+        // Record the position of the initial click
+        world.set<MouseState>({.startPosition = mousePressed->position});
+      } else if (auto* mouseReleased = event->getIf<sf::Event::MouseButtonReleased>()) {
+        ShotParticleOnMouseReleased(world, mouseReleased);
+        world.remove<VerticesRenderable>();
       }
     }
 
